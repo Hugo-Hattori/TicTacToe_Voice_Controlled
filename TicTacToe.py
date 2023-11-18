71f4c78954e1a9fe55c61ff019df719d234fc80a
Hugo-Hattori <136493140+Hugo-Hattori@users.noreply.github.com>
2023-11-18 03:00:28 -0300

    project overhaul
    Restructuring project inspired by:
    https://www.geeksforgeeks.org/tic-tac-toe-gui-in-python-using-pygame/

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,44 +1,21 @@
 import pygame
-import os
+import sys
+import time
+from pygame.locals import *
 
+
+#variáveis globais
+SIMBOLO_XO = 'x'
+
+VENCEDOR = None
+
+EMPATE = None
 
 TELA_LARGURA = 500
 TELA_ALTURA = 500
 
-IMAGEM_BACKGROUND = pygame.transform.scale2x(pygame.image.load((os.path.join('imgs', 'malha3x3.png'))))
-IMAGEM_X = pygame.transform.scale2x(pygame.image.load((os.path.join('imgs', 'simbolo-x.png'))))
-IMAGEM_O = pygame.transform.scale2x(pygame.image.load((os.path.join('imgs', 'simbolo-o.png'))))
+FUNDO = (255, 255, 255) #cor padrão da janela
 
-pygame.font.init()
-FONTE_TEXTO = pygame.font.SysFont('arial', 30)
+COR_LINHA = (0, 0, 0) #cor das linhas divisórias
 
-
-class Simbolo_X:
-    IMAGEM = IMAGEM_X
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def desenhar(self, tela):
-        tela.blit(self.IMAGEM, (self.x, self.y))
-
-
-class Simbolo_O:
-    IMAGEM = IMAGEM_O
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def desenhar(self, tela):
-        tela.blit(self.IMAGEM, (self.x, self.y))
-
-
-def desenhar_tela(tela, simbolos_o, simbolos_x):
-    tela.blit(IMAGEM_BACKGROUND, (0, 0))
-    #desenhar X ou O de acordo com o comando
-    pygame.display.update()
-
-
-tela = pygame.display.set_mode((TELA_LARGURA, TELA_ALTURA))
+TABULEIRO = [[None*3], [None*3], [None*3]]
