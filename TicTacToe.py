2c515575ac88d1a7df4e8d8a6d0c4b098bbd013d
Hugo-Hattori <136493140+Hugo-Hattori@users.noreply.github.com>
2023-11-18 03:25:07 -0300

    initial window and imgs configs

--- TicTacToe.py
+++ TicTacToe.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import time
 from pygame.locals import *
+import os
 
 
 #variáveis globais
@@ -19,3 +20,22 @@ FUNDO = (255, 255, 255) #cor padrão da janela
 COR_LINHA = (0, 0, 0) #cor das linhas divisórias
 
 TABULEIRO = [[None*3], [None*3], [None*3]]
+
+#inicializando janela
+pygame.init()
+
+#configurações
+fps = 30
+relogio = pygame.time.Clock()
+tela = pygame.display.set_mode((TELA_LARGURA, TELA_ALTURA), 0, 32)
+pygame.display.set_caption('Tic Tac Toe')
+
+#carregando imagens
+IMAGEM_BACKGROUND = pygame.image.load((os.path.join('imgs', 'malha3x3.png')))
+IMAGEM_X = pygame.image.load((os.path.join('imgs', 'simbolo-x.png')))
+IMAGEM_O = pygame.image.load((os.path.join('imgs', 'simbolo-o.png')))
+
+#redimensionando imagens
+IMAGEM_BACKGROUND = pygame.transform.scale(IMAGEM_BACKGROUND, (TELA_LARGURA, TELA_ALTURA))
+IMAGEM_X = pygame.transform.scale(IMAGEM_X, (80, 80))
+IMAGEM_O = pygame.transform.scale(IMAGEM_O, (80, 80))
